86ec21dae12ad4922ebbbf19accf372c8552b771
Safin Ali <safin.ali.7205@gmail.com>
2023-03-21 20:17:28 +0600

    routes setup

---
+++ hero-rider/src/routes/Router.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import RegForm from '../Components/form/signup/Rider-Reg-Form';
+import SignUpOption from '../pages/auth/Type-Option-Box';
+
+function Router () {
+
+ return (
+    <BrowserRouter>
+        <Routes>
+            <Route path={`/signup`} element={<SignUpOption/>}/>
+            <Route path={`/signup/:type`} element={<RegForm/>}/>
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
