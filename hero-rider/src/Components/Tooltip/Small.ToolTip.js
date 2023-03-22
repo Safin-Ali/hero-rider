53215a95cc6c9651b3697891e91610eba0b7df1f
Safin Ali <safin.ali.7205@gmail.com>
2023-03-22 21:44:03 +0600

    simple tooltip component created

---
+++ hero-rider/src/Components/Tooltip/Small.ToolTip.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import smallToolTip from './tooltip.module.css'
+
+function SmallToolTip({
+    children, callback = () => {},
+    visible = false
+}) {
+
+    return (
+        <>
+        <div onClick={callback} className={`absolute z-[2] bottom-[-120%] left-[60%] transform -translate-x-1/2 duration-200 ${visible ? 'visible' : 'invisible'}`}>
+            <div className={smallToolTip['small-tooltip']}>
+                    {children}
+                    <span className={smallToolTip['small-tooltip-arrow']}></span>
+                </div>
+        </div>
+        </>
+    );
+};
+
+SmallToolTip.propTypes = {
+    children: PropTypes.string
+}
+export default SmallToolTip;
