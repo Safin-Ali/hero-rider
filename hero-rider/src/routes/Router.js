9396b9ae8781e668ef0d0356710db530746dbb55
Safin Ali <safin.ali.7205@gmail.com>
2023-03-22 21:47:35 +0600

    created root path as a user dashboard

--- hero-rider/src/routes/Router.js
+++ hero-rider/src/routes/Router.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import SignIn from '../Components/form/signin/SignIn';
 import RegForm from '../Components/form/signup/Reg-Form';
 import DashTable from '../Components/Table/Dash.Table';
 import SignUpOption from '../pages/auth/Type-Option-Box';
+import MainPage from '../pages/main/Main.Page';
+import UserRolePrivate from '../pages/private/User.Role.Private';
 
 function Router () {
 
  return (
     <BrowserRouter>
         <Routes>
+            <Route path={`/`} element={<MainPage/>}>
+              <Route index element={<UserRolePrivate/>}/>
+            </Route>
             <Route path={`/signup`} element={<SignUpOption/>}/>
+            <Route path={`/login`} element={<SignIn/>}/>
             <Route path={`/signup/:type`} element={<RegForm/>}/>
             <Route path={`/dashboard`} element={<DashTable/>}/>
         </Routes>
